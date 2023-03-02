558be914567f9c54fd28de7f568ec4cb5b5d3423
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2023-03-02 16:32:28 +0500

    Application: Adjusted sender message width and contact name width in the message bar

--- src/components/MessageBar.jsx
+++ src/components/MessageBar.jsx
@@ -6,10 +6,10 @@ import { profileImage } from "../helpers/profileImage";
 const MessageBar = (props) => {
   return (
     <div className="row heading">
-      <div className="col-sm-2 col-md-1 col-xs-3 heading-avatar main-page-div">
+      <div className="col-sm-2 col-md-1 col-xs-3 heading-avatar main-page-div w-100">
         <div className="heading-avatar-icon main-page-div">
           <img src={(profileImage(props.contact))} />
-          <span className="mx-2 main-page-span">{props.contact.name}</span>
+          <span className="mx-2 main-page-span w-100">{props.contact.name}</span>
         </div>
       </div>
     </div>
